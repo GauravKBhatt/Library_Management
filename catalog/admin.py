11421ad3b0e2b9f566787174061c3e2d3d306ec3
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-03-09 14:12:39 +0545

    Finished Django Tutorial Part 4, Customizing the admin pannel.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # since genre field is many to many field, to reduce large database access "cost", we use display_genre function.
+    list_display=('title','language','author','display_genre')
+    inlines = [BooksInstanceInline]
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display=('id','due_back')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    # before writing the list display the admin pannel was using the string generated from the model __str__() method.
+    list_display=('last_name','first_name','date_of_birth','date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines=[BookInline]
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
