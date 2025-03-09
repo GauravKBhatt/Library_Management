9d6340210e791f61caec836065b95123f4cd3075
Gaurav Bhatt <gauravkapadibhatt@gmail.com>
2025-03-09 16:31:12 +0545

    Finished Templating, challenges left

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-
+    path('',HomeView.as_view(),name="Home"),
+    path('home/',HomeView.as_view(),name="Home"),
+    path('books/',BooksView.as_view(),name="Books"),
+    path('authors/',AuthorsView.as_view(),name="Authors"),
+    path('book/<str:pk>',BookView.as_view(),name="Book"),
+    path('author/<str:pk>',AuthorView.as_view(),name="Author"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.views import View
+from .models import *
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self,request,*args,**kwargs):
+        num_books=Book.objects.all().count()
+        num_instances=BookInstance.objects.all().count()
+        num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+        num_authors=Author.objects.count()
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors
+        }
+        return render(request,'catalog/home.html',context)
+
+class BooksView(View):
+    def get(self,request,*args,**kwargs):
+        books=Book.objects.all()
+        context={"books":books}
+        return render(request,'catalog/books.html',context)
+    
+class BookView(View):
+    def get(self,request,*args,**kwargs):
+        pk=kwargs.get('pk')
+        book_instance=Book.objects.get(id=pk)
+        context={"book":book_instance}
+        return render(request,'catalog/book.html',context)
+    
+class AuthorsView(View):
+    def get(self,request,*args,**kwargs):
+        authors=Author.objects.all()
+        context={"authors":authors}
+        return render(request,'catalog/authors.html',context)
+    
+class AuthorView(View):
+    def get(self,request,*args,**kwargs):
+        pk=kwargs.get('pk')
+        author_instance=Author.objects.get(id=pk)
+        context={"book":author_instance}
+        return render(request,'catalog/author.html',context)
